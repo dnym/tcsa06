23e6989aead060b806e1d5566fc9b9888504270b
David Nyman <david.nyman@protonmail.com>
2023-11-28 08:03:23 +0100

    Add mock

---
+++ DrinksInfo.DataAccess/Mock.cs
@@ -0,0 +1,25 @@
+﻿using DrinksInfo.DataAccess.Models;
+
+namespace DrinksInfo.DataAccess;
+
+public class Mock : IDataAccess
+{
+    public Task<List<Category>> GetCategoriesAsync()
+    {
+        var categories = new List<Category>
+        {
+            new("Beer"),
+            new("Cocktail"),
+            new("Cocoa"),
+            new("Coffee / Tea"),
+            new("Homemade Liqueur"),
+            new("Ordinary Drink"),
+            new("Punch / Party Drink"),
+            new("Shake"),
+            new("Shot"),
+            new("Soft Drink"),
+            new("Other / Unknown")
+        };
+        return Task.FromResult(categories);
+    }
+}
