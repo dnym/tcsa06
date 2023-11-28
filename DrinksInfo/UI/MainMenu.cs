89a8a2cf1a242a367f423d67df15e8233b0be1ce
David Nyman <david.nyman@protonmail.com>
2023-11-28 07:27:30 +0100

    Begin using helper

--- DrinksInfo/UI/MainMenu.cs
+++ DrinksInfo/UI/MainMenu.cs
@@ -6,7 +6,51 @@ internal static class MainMenu
 {
     public static Screen Get()
     {
-        Screen screen = new();
+        const string menuContents = @"+-----CATEGORIES------+
+| Beer                |
++---------------------+
+| Cocktail            |
++---------------------+
+| Cocoa               |
++---------------------+
+| Coffee / Tea        |
++---------------------+
+| Homemade Liqueur    |
++---------------------+
+| Ordinary Drink      |
++---------------------+
+| Punch / Party Drink |
++---------------------+
+| Shake               |
++---------------------+
+| Shot                |
++---------------------+
+| Soft Drink          |
++---------------------+
+| Other / Unknown     |
++---------------------+";
+        SelectionMenu? menu = null;
+        int previousUsableHeight = -1;
+
+        Screen screen = new(body: (_, usableHeight) =>
+        {
+            if (usableHeight != previousUsableHeight)
+            {
+                menu = new(menuContents, itemCount: 11, indexToLine: (i) => 1 + (2 * i), leftIndicator: ">>", rightIndicator: "<<", startSelectedIndex: menu?.SelectedIndex ?? 0, maxHeight: usableHeight);
+            }
+            previousUsableHeight = usableHeight;
+
+            return menu!.Show();
+        });
+        screen.AddAction(ConsoleKey.UpArrow, () => menu!.SelectedIndex--);
+        screen.AddAction(ConsoleKey.DownArrow, () => menu!.SelectedIndex++);
+        screen.AddAction(ConsoleKey.PageUp, () => menu!.SelectedIndex -= 5);
+        screen.AddAction(ConsoleKey.PageDown, () => menu!.SelectedIndex += 5);
+        screen.AddAction(ConsoleKey.Home, () => menu!.SelectedIndex = 0);
+        screen.AddAction(ConsoleKey.End, () => menu!.SelectedIndex = 10);
+
+        screen.AddAction(ConsoleKey.RightArrow, () => Console.WriteLine($"{menuContents.Split("\n")[1 + (2 * menu!.SelectedIndex)]}"));
+
         return screen;
     }
 }
